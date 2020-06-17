614d703ba6d259fc555ad554d73e347fc7a378dd
leon6002 <uomleon@163.com>
2020-06-17 15:12:43 +0800

    initial commit for the project

---
+++ product/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from product.models import Product
+from apitest.models import Apitest, Apis
+
+
+# Register your models here.
+
+
+class ApisAdmin(admin.TabularInline):
+    list_display = ['apiname', 'apiurl', 'apiparamvalue', 'apimethod', 'apiresult', 'apistatus', 'create_time',
+                    'id', 'product']
+    model = Apis
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['product_name', 'productdesc', 'producter', 'create_time', 'id']
+    inlines = [ApisAdmin]
+
+
+admin.site.register(Product)  # 把产品模块注册到Django admin后台并能显示
